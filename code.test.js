b914b898246161388ec1c2eeea27211d2a87d603
Ryan Zafft <123418615+rzafft1@users.noreply.github.com>
2023-10-19 18:09:29 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const jsc = require('jsverify');
+const assert = require('assert');
 
-// Read and evaluate code from 'code.js'
-const codeContent = fs.readFileSync('code.js', 'utf8');
-eval(codeContent);
+eval(fs.readFileSync('code.js')+'');
 
 var test1 = [
     [0, 1, 0, 1, 0],
@@ -13,33 +11,10 @@ var test1 = [
     [1, 1, 0, 0, 0]
 ];
 
-if (depthFirstSearch(test1, 0, 1).length < 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
-
-if (depthFirstSearch(test1, 0, 2).length > 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
-
-if (depthFirstSearch(test1, 0, 3).length < 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
-
-if (depthFirstSearch(test1, 0, 4).length < 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
+assert(JSON.stringify(depthFirstSearch(test1, 0, 1)) != JSON.stringify([])) 
+assert(JSON.stringify(depthFirstSearch(test1, 0, 2)) == JSON.stringify([]))  
+assert(JSON.stringify(depthFirstSearch(test1, 0, 3)) != JSON.stringify([])) 
+assert(JSON.stringify(depthFirstSearch(test1, 0, 4)) != JSON.stringify([])) 
 
 var test2 = [
     [0,0,0,0,0],
@@ -49,33 +24,10 @@ var test2 = [
     [0,0,0,1,0]
 ]
 
-if (depthFirstSearch(test2, 0, 4).length > 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
-
-if (depthFirstSearch(test2, 2, 3).length < 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
-
-if (depthFirstSearch(test2, 1, 0).length < 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
-
-if (depthFirstSearch(test2, 3, 4).length < 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
+assert(JSON.stringify(depthFirstSearch(test2, 0, 1)) == JSON.stringify([])) 
+assert(JSON.stringify(depthFirstSearch(test2, 2, 3)) != JSON.stringify([])) 
+assert(JSON.stringify(depthFirstSearch(test2, 1, 0)) != JSON.stringify([])) 
+assert(JSON.stringify(depthFirstSearch(test2, 3, 4)) != JSON.stringify([])) 
 
 var test3 = [
     [0,0,0,0,1],
@@ -85,30 +37,7 @@ var test3 = [
     [1,0,1,0,0]
 ]
 
-if (depthFirstSearch(test3, 0, 3).length < 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
-
-if (depthFirstSearch(test3, 1, 4).length > 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
-
-if (depthFirstSearch(test3, 2, 0).length > 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
-
-if (depthFirstSearch(test3, 4, 3).length < 1) {
-    console.log("ERROR")
-}
-else {
-    console.log("PASSED")
-}
+assert(JSON.stringify(depthFirstSearch(test3, 0, 3)) != JSON.stringify([])) 
+assert(JSON.stringify(depthFirstSearch(test3, 1, 4)) == JSON.stringify([])) 
+assert(JSON.stringify(depthFirstSearch(test3, 2, 0)) == JSON.stringify([])) 
+assert(JSON.stringify(depthFirstSearch(test3, 4, 3)) != JSON.stringify([])) 
